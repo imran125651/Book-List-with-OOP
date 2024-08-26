c351d997928faa56d288a7684de870ad087a875d
Imran Hossain <imran125651@gmail.com>
2024-08-26 23:45:55 +0600

    javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Get the UI elements
 let form = document.querySelector("#book-form");
+let booklist = document.querySelector("#book-list");
 
 
  
@@ -14,25 +15,28 @@ class Book{
 }
 
 class UI{
-    constructor() {
-        
-    }
-    addToBookList(book){
+    static addToBookList(book){
         let list = document.querySelector("#book-list");
         let row = document.createElement("tr");
         row.innerHTML = `
         <td>${book.title}</td>
-        <td>${book.author}</td>
+        <td>${book.author}</td>  
         <td>${book.isbn}</td>
         <td><a href="#" class="delete">X</a></td>
         `;
         list.appendChild(row);
 
-        console.log(row);
+    }
+
+    static removeBookFromBookList(target){
+        if(target.hasAttribute("href")){
+            target.parentElement.parentElement.remove(); 
+            UI.showAlert("Book Removed!", "success");
+        }
     }
 
 
-    showAlert(message, className) {
+    static showAlert(message, className) {
         let errrDiv = document.createElement("div");
         errrDiv.appendChild(document.createTextNode(message));
         errrDiv.className = `alert ${className}`;
@@ -44,16 +48,16 @@ class UI{
             errrDiv.remove();
         }, 3000);
        
-
-
-        console.log(container);
     }
 }
 
 
 
 // Add Event Listener
+
+document.addEventListener("DOMContentLoaded", Store.displayBooks());
 form.addEventListener("submit", newBook);
+booklist.addEventListener("click", removeBook);
 
 
 
@@ -66,16 +70,19 @@ function newBook(e){
     let author = document.querySelector("#author").value;
     let isbn = document.querySelector("#isbn").value;
 
-    let ui = new UI();
 
     if(title === "" || author === "" || isbn === ""){
-        ui.showAlert("Please fill all the fields.", "error");
+        UI.showAlert("Please fill all the fields.", "error");
     }
     else{
         let book = new Book(title, author, isbn);
+        
 
-        ui.addToBookList(book);
-        ui.showAlert("Book is added successfully", "success");
+        UI.addToBookList(book);
+        UI.showAlert("Book is added successfully", "success");
+
+        
+        Store.addNewBook(book);
         clearFields();
     }    
 
@@ -83,8 +90,41 @@ function newBook(e){
 }
 
 
+function removeBook(e){
+    UI.removeBookFromBookList(e.target);
+
+    e.preventDefault();
+}
+
+
 function clearFields(){
     document.querySelector("#title").value = "";
     document.querySelector("#author").value = "";
     document.querySelector("#isbn").value = "";
+}
+
+
+class Store{
+    static getBooks(){
+        let books;
+        if(localStorage.getItem("books") === null){
+            books = [];
+        }
+        else{
+            books = JSON.parse(localStorage.getItem("books"));
+        }
+        return books;
+    }
+
+
+    static addNewBook(book){
+        let allBooks = Store.getBooks();
+        allBooks.push(book);
+        localStorage.setItem("books", JSON.stringify(allBooks));
+    }
+
+
+    static displayBooks(){
+        console.log("display books");
+    }
 }
